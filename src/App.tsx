77fef77098927a6d478d90955ec78bcdeedfec7e
Julio Güella <julio.guella@artekium.com>
2021-12-29 12:06:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
+
+import { Article } from "./components/Article"
+import { AddArticle } from "./components/AddArticle"
+import { addArticle, removeArticle } from "./store/actionCreators"
+import { Dispatch } from "redux"
+
+const App: React.FunctionComponent = () => {
+  const articles: IArticle[] = useSelector(
+    (state: ArticleState) => state.articles,
+    shallowEqual
+  )
+
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const saveArticle = React.useCallback(
+    (article: IArticle) => dispatch(addArticle(article)),
+    [dispatch]
+  )
+
+  return (
+    <main>
+      <h1>My Articles</h1>
+      <AddArticle saveArticle={saveArticle} />
+      {articles.map((article: IArticle) => (
+        <Article
+          key={article.id}
+          article={article}
+          removeArticle={removeArticle}
+        />
+      ))}
+    </main>
+  )
+}
+
+export default App
